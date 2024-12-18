2f51de314d96a20c6f0caef6954caa2938cd73f7
Alfred J <80150511+AFutureAndroid@users.noreply.github.com>
2024-12-17 21:51:40 -0700

    Part 1 of day 1 is complete!

--- Day1.py
+++ Day1.py
@@ -1,13 +1,23 @@
+# Day 1 of Advent of Code
 
 f = open("Day1.txt", "r")
 list1 = []
 list2 = []
+test = 0
 for line in f:
     line = line.split()
     list1.append(line[0])
     list2.append(line[1])
-    print(list1.size())
-    
 
+list1 = sorted(list1)
+list2 = sorted(list2)
+total = 0
+index = 0
 
+for i in list1:
+    diff = abs(int(list1[index]) - int(list2[index]))
+    total += diff
+    index += 1
+
+print(total)
 
